1ff3f47434f7e8e84f0a6a240c449e07bd4f7e94
Megan Ford <meg.c.ford@gmail.com>
2016-07-27 11:21:14 -0500

    area logic functions all work

--- sharpshapes.py
+++ sharpshapes.py
@@ -3,70 +3,76 @@ import math
 class Shape:
     def __init__(self):
         self.area = 0
+        # TODO: make the 'calculate area function run in the init?'
 
 
 class Solid:
     def __init__(self):
         self.volume = 0
+        # TODO: make the 'calculate area function run in the init?'
 
+
+##########SHAPES##########
 
 class Rectangle(Shape):
-    def calculate_rectangle_area(self, length, height):
-        self.area = length * height
+    def calculate_area(self, length, width):
+        self.area = length * width
 
 
 class Square(Rectangle):
-    def calculate_square_area(self, side):
-        self.calculate_rectangle_area(side, side)
+    def calculate_area(self, side):
+        super().calculate_area(side, side)
 
 
 class Rhombus(Shape):
-    def calculate_rhombus_area(self, diagonal_one, diagonal_two):
-        self.area = (diagonal_one * diagonal_two) / 2
+    def calculate_area(self, diagonal_one, diagonal_two):
+        self.area = diagonal_one * diagonal_two
 
 
 class Parallelogram(Shape):
-    def calculate_parallelogram_area(self, base, height):
+    def calculate_area(self, base, height):
         self.area = base * height
 
 
 class Triangle(Shape):
-    def calculate_triangle_area(self, base, height):
+    def calculate_area(self, base, height):
         self.area = (base * height) / 2
 
 
 class Circle(Shape):
-    def calculate_circle_area(self, radius):
-        self.area = math.pi * (radius * radius)
+    def calculate_area(self, radius):
+        self.area = round(((radius * radius) * math.pi), 2)
 
+
+#######SOLIDS#######
 
 class Pyramid(Solid):
-    def calculate_pyramid_volume(self):
-        pass
+    def calculate_volume(self):
+        self.volume = 4
 
 
 class Cone(Solid):
 
-    def calculate_cone_volume(self):
-        pass
+    def calculate_volume(self):
+        self.volume = 4
 
 
 class Cylinder(Solid):
 
-    def calculate_cylinder_volume(self):
-        pass
+    def calculate_volume(self):
+        self.volume = 4
 
 
 class Sphere(Solid):
-    def calculate_sphere_volume(self):
-        pass
+    def calculate_volume(self):
+        self.volume = 4
 
 
 class Block(Solid):
-    def calculate_block_volume(self):
-        pass
+    def calculate_volume(self):
+        self.volume = 4
 
 
 class Cube(Block):
-    def calculate_cube_volume(self):
-        pass
+    def calculate_volume(self):
+        self.volume = 4
--- test_shapes.py
+++ test_shapes.py
@@ -16,64 +16,88 @@ class Test_shapes(unittest.TestCase):
         test_rectangle = Rectangle()
         self.assertIsInstance(test_rectangle, Rectangle)
         self.assertIsInstance(test_rectangle, Shape)
+        self.assertEqual(test_rectangle.area, 0)
+        test_rectangle.calculate_area(2, 2)
+        self.assertEqual(test_rectangle.area, 4)
 
     def test_square_structure(self):
         test_square = Square()
         self.assertIsInstance(test_square, Square)
         self.assertIsInstance(test_square, Rectangle)
         self.assertIsInstance(test_square, Shape)
+        self.assertEqual(test_square.area, 0)
+        test_square.calculate_area(4)
+        self.assertEqual(test_square.area, 16)
 
     def test_rhombus_structure(self):
         test_rhombus = Rhombus()
         self.assertIsInstance(test_rhombus, Rhombus)
         self.assertIsInstance(test_rhombus, Shape)
+        self.assertEqual(test_rhombus.area, 0)
+        test_rhombus.calculate_area(2, 2)
+        self.assertEqual(test_rhombus.area, 4)
 
     def test_parallelogram_structure(self):
         test_parallelogram = Parallelogram()
         self.assertIsInstance(test_parallelogram, Parallelogram)
         self.assertIsInstance(test_parallelogram, Shape)
+        self.assertEqual(test_parallelogram.area, 0)
+        test_parallelogram.calculate_area(2, 2)
+        self.assertEqual(test_parallelogram.area, 4)
 
     def test_triangle_structure(self):
         test_triangle = Triangle()
         self.assertIsInstance(test_triangle, Triangle)
         self.assertIsInstance(test_triangle, Shape)
+        self.assertEqual(test_triangle.area, 0)
+        test_triangle.calculate_area(4, 2)
+        self.assertEqual(test_triangle.area, 4)
 
     def test_circle_structure(self):
         test_circle = Circle()
         self.assertIsInstance(test_circle, Circle)
         self.assertIsInstance(test_circle, Shape)
+        self.assertEqual(test_circle.area, 0)
+        test_circle.calculate_area(4)
+        self.assertEqual(test_circle.area, 50.27)
 
     # ============solids=============
     def test_pyramid_structure(self):
         test_pyramid = Pyramid()
         self.assertIsInstance(test_pyramid, Pyramid)
         self.assertIsInstance(test_pyramid, Solid)
+        self.assertEqual(test_pyramid.volume, 0)
 
     def test_cone_structure(self):
         test_cone = Cone()
         self.assertIsInstance(test_cone, Cone)
         self.assertIsInstance(test_cone, Solid)
+        self.assertEqual(test_cone.volume, 0)
 
     def test_cylinder_structure(self):
         test_cylinder = Cylinder()
         self.assertIsInstance(test_cylinder, Cylinder)
         self.assertIsInstance(test_cylinder, Solid)
+        self.assertEqual(test_cylinder.volume, 0)
 
     def test_sphere_structure(self):
         test_sphere = Sphere()
         self.assertIsInstance(test_sphere, Sphere)
         self.assertIsInstance(test_sphere, Solid)
+        self.assertEqual(test_sphere.volume, 0)
 
     def test_block_structure(self):
         test_block = Block()
         self.assertIsInstance(test_block, Block)
         self.assertIsInstance(test_block, Solid)
+        self.assertEqual(test_block.volume, 0)
 
     def test_cube_structure(self):
         test_cube = Cube()
         self.assertIsInstance(test_cube, Block)
         self.assertIsInstance(test_cube, Cube)
         self.assertIsInstance(test_cube, Solid)
+        self.assertEqual(test_cube.volume, 0)
 
 if __name__ == '__main__':
     unittest.main()
