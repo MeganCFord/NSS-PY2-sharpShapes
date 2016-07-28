c3ee0ccc91ee79eab7f5f353191d2b2dae2f742e
Megan Ford <meg.c.ford@gmail.com>
2016-07-28 10:18:39 -0500

    added doc strings and comments

--- menu.py
+++ menu.py
@@ -1,13 +1,10 @@
 from sharpshapes import *
 
-
 class Sharp_Shapes:
     def __init__(self):
-
-        # run the start menu on init.
-        self.start_menu()
         self.current_shape = None
 
+    # Initial Start Menu with two options. TODO: error handle inputs.
     def start_menu(self):
         intro = "==========SHARP SHAPES==========\ncalculate the area of a shape or volume of a solid!"
         print(intro)
@@ -19,6 +16,7 @@ class Sharp_Shapes:
         elif intro_input == "2":
             self.solid_menu()
 
+    # secondary menu for (2D) shapes. TODO: Error handle inputs.
     def shape_menu(self):
         shape_string = "1. Rectangle\n2. Square\n3. Rhombus\n4. Parallelogram\n5. Triangle\n6. Circle\n7. Go Back"
         print(shape_string)
@@ -26,83 +24,89 @@ class Sharp_Shapes:
         if instantiation_input not in ("1", "2", "3", "4", "5", "6", "7"):
             print("please type the number of the shape you'd like to calculate.")
         elif instantiation_input == "1":
+            # for each of these options, first input for the required size-finding measurements is requested.
+            # Then the shape class is instantiated with the measurements passed in.
+            # Then the area is calculated, which is then printed-
+            # then the start menu is re-displayed to start again.
             length = int(input("enter the length of your rectangle. "))
             width = int(input("enter the width of your rectangle. "))
             self.current_shape = Rectangle(length, width)
-            self.current_shape.calculate_area()
-            self.start_menu()
+
         elif instantiation_input == "2":
             side = int(input("enter the length of the sides of your square. "))
             self.current_shape = Square(side)
-            self.current_shape.calculate_area()
-            self.start_menu()
+
         elif instantiation_input == "3":
             diagonal_one = int(input("enter the first diagonal of your rhombus. "))
             diagonal_two = int(input("enter the second diagonal of your rhombus. "))
             self.current_shape = Rhombus(diagonal_one, diagonal_two)
-            self.current_shape.calculate_area()
-            self.start_menu()
+
         elif instantiation_input == "4":
             base = int(input("enter the base of your parallelogram. "))
             height = int(input("enter the height of your parallelogram. "))
             self.current_shape = Parallelogram(base, height)
-            self.current_shape.calculate_area()
-            self.start_menu()
+
         elif instantiation_input == "5":
             base = int(input("enter the base of your triangle. "))
             height = int(input("enter the height of your triangle. "))
             self.current_shape = Triangle(base, height)
-            self.current_shape.calculate_area()
-            self.start_menu()
+
         elif instantiation_input == "6":
             radius = int(input("enter the radius of your circle. "))
             self.current_shape = Circle(radius)
-            self.current_shape.calculate_area()
-            self.start_menu()
+
         elif instantiation_input == "7":
                 self.start_menu()
 
+        self.current_shape.calculate_area()
+        print(self.current_shape.__str__())
+        self.start_menu()
+
+    # secondary menu for (2D) shapes. TODO: Error handle inputs
     def solid_menu(self):
         solid_string = "1. Pyramid\n2. Cone\n3. Cylinder\n4. Sphere\n5. Block\n6. Cube\n7. Go Back"
         print(solid_string)
         instantiation_input = input()
         if instantiation_input not in ("1", "2", "3", "4", "5", "6", "7"):
             print("please type the number of the solid you'd like to calculate.")
+            # This exits the program which I don't want.
         elif instantiation_input == "1":
+            # for each of these options, first: input for the required size-finding measurements is requested.
+            # Then the shape class is instantiated with the measurements passed in.
+            # Then the area is calculated, which is then printed-
+            # then the start menu is re-displayed to start again.
             base_length = int(input("enter the base length of your pyramid. "))
             base_width = int(input("enter the base width of your pyramid. "))
             height = int(input("enter the height of your pyramid. "))
             self.current_shape = Pyramid(base_length, base_width, height)
-            self.current_shape.calculate_volume()
-            self.start_menu()
+
         elif instantiation_input == "2":
             base_radius = int(input("enter the base radius of your cone. "))
             height = int(input("enter the height of your cone. "))
             self.current_shape = Cone(base_radius, height)
-            self.current_shape.calculate_volume()
-            self.start_menu()
+
         elif instantiation_input == "3":
             base_radius = int(input("enter the base radius of your cylinder. "))
             height = int(input("enter the height of your cylinder. "))
             self.current_shape = Cylinder(base_radius, height)
-            self.current_shape.calculate_volume()
-            self.start_menu()
+
         elif instantiation_input == "4":
             radius = int(input("enter the radius of your sphere. "))
             self.current_shape = Sphere(radius)
-            self.current_shape.calculate_volume()
-            self.start_menu()
+
         elif instantiation_input == "5":
             base_length = int(input("enter the base length of your block. "))
             base_width = int(input("enter the base width of your block. "))
             height = int(input("enter the height of your block. "))
             self.current_shape = Block(base_length, base_width, height)
-            self.current_shape.calculate_volume()
-            self.start_menu()
+
         elif instantiation_input == "6":
             side = int(input("enter the length of the sides of your cube. "))
             self.current_shape = Cube(side)
-            self.current_shape.calculate_volume()
-            self.start_menu()
+
         elif instantiation_input == "7":
                 self.start_menu()
+
+        self.current_shape.calculate_volume()
+        print(self.current_shape.__str__())
+        self.start_menu()
--- sharpshapes.py
+++ sharpshapes.py
@@ -1,27 +1,26 @@
 import math
 from menu import *
 
+
+# master class.
 class Shape():
     def __init__(self):
-        # self.name = "shape"
         self.area = 0
 
     def __str__(self):
         return("the area of your " + self.name + " is " + str(self.area) + ".")
 
 
-
+# master class.
 class Solid():
     def __init__(self):
-        # self.name = "solid"
         self.volume = 0
 
     def __str__(self):
         return("the volume of your " + self.name + " is " + str(self.volume) + ".")
 
-    
-##########SHAPES##########
 
+# #########SHAPES##########
 class Rectangle(Shape):
 
     def __init__(self, length, width):
@@ -31,10 +30,16 @@ class Rectangle(Shape):
         self.name = "rectangle"
 
     def calculate_area(self):
+        '''
+        Calculates the area of a rectangle based on the length and width specified at the time of rectangle class instantiation.
+
+        Arguments: none
+        '''
         self.area = round((self.length * self.width), 2)
-        print(self.__str__())
+
 
 class Square(Rectangle):
+    # note that since square inherits from rectangle, it does not have its own calculate_area method.
     def __init__(self, side):
         super().__init__(side, side)
         self.name = "square"
@@ -48,8 +53,12 @@ class Rhombus(Shape):
         self.name = "rhombus"
 
     def calculate_area(self):
+        '''
+        Calculates the area of a Rhombus based on the two diagonal measurements specified at the time of Rhombus class instantiation.
+
+        Arguments: none
+        '''
         self.area = round((self.diagonal_one * self.diagonal_two), 2)
-        print(self.__str__())
 
 
 class Parallelogram(Shape):
@@ -60,8 +69,12 @@ class Parallelogram(Shape):
         self.name = "parallelogram"
 
     def calculate_area(self):
+        '''
+        Calculates the area of a parallelogram based on the base and height specified at the time of parallelogram class instantiation.
+
+        Arguments: none
+        '''
         self.area = round((self.base * self.height), 2)
-        print(self.__str__())
 
 
 class Triangle(Shape):
@@ -72,8 +85,12 @@ class Triangle(Shape):
         self.name = "triangle"
 
     def calculate_area(self):
+        '''
+        Calculates the area of a triangle based on the base and height specified at the time of triangle class instantiation.
+
+        Arguments: none
+        '''
         self.area = round(((self.base * self.height) / 2), 2)
-        print(self.__str__())
 
 
 class Circle(Shape):
@@ -83,11 +100,15 @@ class Circle(Shape):
         self.name = "circle"
 
     def calculate_area(self):
-        self.area = round(((self.radius **2) * math.pi), 2)
-        print(self.__str__())
+        '''
+        Calculates the area of a circle based on the radius specified at the time of circle class instantiation.
+
+        Arguments: none
+        '''
+        self.area = round(((self.radius ** 2) * math.pi), 2)
 
 
-#######SOLIDS#######
+# ######SOLIDS#######
 
 class Pyramid(Solid):
     def __init__(self, base_length, base_width, height):
@@ -99,8 +120,12 @@ class Pyramid(Solid):
 
     # NOTE: this calculation works for a rectangular pyramid only
     def calculate_volume(self):
+        '''
+        Calculates the volume of a pyramid based on the base length, base width, and height specified at the time of pyramid class instantiation.
+
+        Arguments: none
+        '''
         self.volume = round(((self.base_length * self.base_width * self.height) / 3), 2)
-        print(self.__str__())
 
 
 class Cone(Solid):
@@ -111,8 +136,12 @@ class Cone(Solid):
         self.height = height
 
     def calculate_volume(self):
-        self.volume = round((((self.base_radius **2) * self.height * math.pi)/3), 2)
-        print(self.__str__())
+        '''
+        Calculates the volume of a cone based on the base radius and height specified at the time of cone class instantiation.
+
+        Arguments: none
+        '''
+        self.volume = round((((self.base_radius ** 2) * self.height * math.pi)/3), 2)
 
 
 class Cylinder(Solid):
@@ -123,8 +152,12 @@ class Cylinder(Solid):
         self.height = height
 
     def calculate_volume(self):
-        self.volume = round(((self.radius **2)*self.height * math.pi), 2)
-        print(self.__str__())
+        '''
+        Calculates the volume of a cylinder based on the base radius and height specified at the time of cylinder class instantiation.
+
+        Arguments: none
+        '''
+        self.volume = round(((self.radius ** 2)*self.height * math.pi), 2)
 
 
 class Sphere(Solid):
@@ -134,8 +167,12 @@ class Sphere(Solid):
         self.radius = radius
 
     def calculate_volume(self):
-        self.volume = round((((self.radius **3)*math.pi*4)/3), 2)
-        print(self.__str__())
+        '''
+        Calculates the volume of a sphere based on the base radius specified at the time of sphere class instantiation.
+
+        Arguments: none
+        '''
+        self.volume = round((((self.radius ** 3)*math.pi*4)/3), 2)
 
 
 class Block(Solid):
@@ -146,15 +183,23 @@ class Block(Solid):
         self.height = height
         self.name = "block"
 
+    # NOTE: this shape works for blocks with all right angles only.
     def calculate_volume(self):
+        '''
+        Calculates the volume of a rectangular based on the base length, base width, and height specified at the time of rectangular class instantiation.
+
+        Arguments: none
+        '''
         self.volume = round((self.length * self.width * self.height), 2)
-        print(self.__str__())
 
 
 class Cube(Block):
+    # note that since cube inherits from block, it does not have its own calculate_area method.
     def __init__(self, side):
         super().__init__(side, side, side)
         self.name = "cube"
 
+
 if __name__ == '__main__':
     app = Sharp_Shapes()
+    app.start_menu()
